daa21333820bffd41ac8c75d100dbcd9c60ef587
jimuium <jihautaluoma@gmail.com>
2025-05-22 19:19:40 +0300

    make actuall rows to  json

--- src/logicFunction/rowsToJSON.go
+++ src/logicFunction/rowsToJSON.go
@@ -37,3 +37,36 @@ func RowsToJSON(rows *sql.Rows) ([]byte, error) {
 
 	return json.Marshal(results)
 }
+
+func RowsToJSONObject(rows *sql.Rows) (map[string]interface{}, error) {
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	var results []map[string]interface{}
+	for rows.Next() {
+		values := make([]interface{}, len(columns))
+		valuePtrs := make([]interface{}, len(columns))
+		for i := range values {
+			valuePtrs[i] = &values[i]
+		}
+
+		if err := rows.Scan(valuePtrs...); err != nil {
+			return nil, err
+		}
+
+		rowMap := make(map[string]interface{})
+		for i, col := range columns {
+			val := values[i]
+			if b, ok := val.([]byte); ok {
+				rowMap[col] = string(b)
+			} else {
+				rowMap[col] = val
+			}
+		}
+		results = append(results, rowMap)
+	}
+
+	return map[string]interface{}{"results": results}, nil
+}
